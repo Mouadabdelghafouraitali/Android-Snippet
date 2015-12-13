9df6aca33ebc5a930c2accb016b312b2fbdde76d
Huy Ng. M <minhhuy150894@users.noreply.github.com>
2015-12-13 21:42:02 +0700

    Update LIST.java

--- LIST.java
+++ LIST.java
@@ -10,10 +10,6 @@ screen.setBackgroundResource(drawable id)
 
 
 
-//********************************************** Download image from internet
-URL url = new URL(link text);
-Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-img.setImageBitmap(bmp);
 
 
 
@@ -74,27 +70,6 @@ new CountDownTimer(5000, 100){
 
 
 
-// // **********************************************  read text / html from internet
-private class GetXML extends AsyncTask <String, Void, String>{
-	@Override
-	protected String doInBackground(String ...urls){
-		String xml = null;
-		xml = getXmlFromUrl("http://....");
-		return xml;
-	}
-	protected void onPostExecute(String xml){
-		XMLDOMParser parser = new XMLDOMParser();
-		Document doc = parser.getDocument(xml);
-		NodeList nodeList = doc.getElementsByTagName("tag name");
-
-		String result = "";
-		for(int i=0; i<nodeList.getLength(); i++){
-			Element e = (Element) nodeList.item(i);
-			result += parser.getValue(e, "name");
-		}
-		Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
-	}
-}
 
 
 
@@ -210,23 +185,6 @@ Stop:
 onPause(); 
 song.pause();
 
-// from internet
-public void PlayMp3FromInternet(String url){
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try {
-            mediaPlayer.setDataSource(url);
-            mediaPlayer.prepareAsync();
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 
@@ -402,103 +360,7 @@ class YourAsync extends AsyncTask <String, Integer, String> {
 
 
 
-// ********************************************** XMLDOMPARSER
-// step 1: create class
-public class XMLDOMParser {
-    public Document getDocument(String xml)
-    { 
-        Document document = null;
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try{
-            DocumentBuilder db = factory.newDocumentBuilder();
-            InputSource is = new InputSource();
-            is.setCharacterStream(new StringReader(xml));
-            is.setEncoding("UTF-8");
-            document = db.parse(is);
-        }catch(ParserConfigurationException e)
-        {
-            Log.e("Error: ", e.getMessage(), e);
-            return null;
-        }
-        catch (SAXException e) {
-            Log.e("Error: ", e.getMessage(), e);
-            return null;
-        }
-        catch(IOException e){
-            Log.e("Error: ", e.getMessage(), e);
-            return null;
-        }
-        return document;
-    }
-    public String getValue(Element item, String name)
-    {
-        NodeList nodes = item.getElementsByTagName(name);
-        return this.getTextNodeValue(nodes.item(0));
-    }
-    private final String getTextNodeValue(Node elem) {
-        Node child;
-        if( elem != null){
-            if (elem.hasChildNodes()){
-                for( child = elem.getFirstChild(); child != null; child = child.getNextSibling() ){
-                    if( child.getNodeType() == Node.TEXT_NODE  ){
-                        return child.getNodeValue();
-                    }
-                }
-            }
-        }
-        return "";
-    }
-}
 
-// step 2: MainActivity
-//      2.1: create method getXmlFromUrl(String urlString)
-private String getXmlFromUrl(String urlString) {
-        String xml = null;
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(urlString);
-
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            xml = EntityUtils.toString(httpEntity, HTTP.UTF_8);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return xml;
-    }
-
-//	    2.2: create Async
-private class GetXMLTask extends AsyncTask <String, Void, String> {
-	@Override
-	protected String doInBackground(String ...urls){
-		String xml = null;
-		xml = getXmlFromUrl("your url here");
-		return xml;
-	}
-	protected void onPostExecute(String xml){
-		XMLDOMParser parser = new XMLDOMParser();
-		Document doc = parser.getDocument(xml);
-		NodeList nodeList = doc.getElementsByTagName("your tag name");
-
-		String result = "";
-		for(int i=0; i<nodeList.getLength(); i++){
-			Element e = (Element) nodeList.item(i);
-			result += parser.getValue(e, "name");
-		}
-		// Use "result"
-	}
-}
-//	    2.3: onCreate:
-runOnUiThread(new Runnable(){
-	@Override
-	public void run(){
-		new GetXMLTask().execute("http url...");
-	}
-})
 
 
 
@@ -527,78 +389,12 @@ protected void onCreate(Bundle savedInstanceState) {
 
 
 
-// ********************************************** send a request to web server
-// use async!
-private String makePostRequest() {
-    HttpClient httpClient = new DefaultHttpClient();
-    
-    // URL of request reciever
-    HttpPost httpPost = new HttpPost("http:... reciever");
-    
-    // params
-    List nameValuePair = new ArrayList(2);
-    nameValuePair.add(new BasicNameValuePair("number1", "111"));
-    nameValuePair.add(new BasicNameValuePair("number2", "222"));
-    
-    //Encoding POST data
-    try {
-        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
-    } catch (UnsupportedEncodingException e) {
-        e.printStackTrace();
-    }
-
-    String result = "";
-    try {
-        HttpResponse response = httpClient.execute(httpPost);
-        HttpEntity entity = response.getEntity();
-        result = EntityUtils.toString(entity);
-    } catch (ClientProtocolException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    return result;
-}
 
 
 
 
 
 
-// ********************************************** read content from an URL from internet
-private static String readFromURL(String theUrl)
-{
-    StringBuilder content = new StringBuilder();
-
-    // many of these calls can throw exceptions, so i've just
-    // wrapped them all in one try/catch statement.
-    try
-    {
-        // create a url object
-        URL url = new URL(theUrl);
-
-        // create a urlconnection object
-        URLConnection urlConnection = url.openConnection();
-
-        // wrap the urlconnection in a bufferedreader
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-        String line;
-
-        // read from the urlconnection via the bufferedreader
-        while ((line = bufferedReader.readLine()) != null)
-        {
-            content.append(line + "\n");
-        }
-        bufferedReader.close();
-    }
-    catch(Exception e)
-    {
-        e.printStackTrace();
-    }
-    return content.toString();
-}
 
 
 
@@ -740,18 +536,7 @@ alert.show();
 
 
 
-// ********************************************** CHECK IF INTERNET IS AVAILABLE
-private boolean haveInternet(){
-	NetworkInfo info = ((ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-	if (info==null || !info.isConnected()) {
-		return false;
-	}
-	if (info.isRoaming()) {
-	// here is the roaming option you can change it if you want to disable internet while roaming, just return false
-		return true;
-	}
-	return true;
-}
+
 
 
 
