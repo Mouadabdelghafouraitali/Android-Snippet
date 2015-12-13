f905361a97625f59332e6694d67cb4baa3c8efd5
Huy Ng. M <minhhuy150894@users.noreply.github.com>
2015-12-13 23:31:46 +0700

    Update local-database-sharepreferrence-file-stream-blabla.java

--- local-database-sharepreferrence-file-stream-blabla.java
+++ local-database-sharepreferrence-file-stream-blabla.java
@@ -11,6 +11,40 @@ song.pause();
 
 
 
+// convert file from LOCAL to byte array
+public byte[] FileLocal_To_Byte(String path){
+        File file = new File(path);
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
+        try {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return bytes;
+}
+
+// path
+outputFile = Environment.getExternalStorageDirectory().
+                getAbsolutePath() + "/filename.3gpp";
+
+filename.3gpp is located in "res" folder.
+
+
+
+
+
+
+
+
+
+
 
 // ********************************************** save and read file txt
 // save
