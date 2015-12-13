25dc60418b3b4a91e05c2ae7492de16eecadb123
Huy Ng. M <minhhuy150894@users.noreply.github.com>
2015-12-13 13:09:28 +0700

    Update download-bitmap-from-url.java

--- download-bitmap-from-url.java
+++ download-bitmap-from-url.java
@@ -1,3 +1,4 @@
+// Method 1:
 public Bitmap getBitmapFromURL(String src) {
         try {
             java.net.URL url = new java.net.URL(src);
@@ -13,3 +14,58 @@ public Bitmap getBitmapFromURL(String src) {
             return null;
         }
     }
+
+// method 2:
+private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+}
+
+// Method 3:
+public static Bitmap loadBitmap(String url) {
+    Bitmap bitmap = null;
+    InputStream in = null;
+    BufferedOutputStream out = null;
+
+    try {
+        in = new BufferedInputStream(new URL(url).openStream(), IO_BUFFER_SIZE);
+
+        final ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+        out = new BufferedOutputStream(dataStream, IO_BUFFER_SIZE);
+        copy(in, out);
+        out.flush();
+
+        final byte[] data = dataStream.toByteArray();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //options.inSampleSize = 1;
+
+        bitmap = BitmapFactory.decodeByteArray(data, 0, data.length,options);
+    } catch (IOException e) {
+        Log.e(TAG, "Could not load Bitmap from: " + url);
+    } finally {
+        closeStream(in);
+        closeStream(out);
+    }
+
+    return bitmap;
+}
