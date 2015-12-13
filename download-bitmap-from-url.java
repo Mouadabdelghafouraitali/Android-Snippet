0f182a52bf5eb04873ab5cd15539f276fe4a705f
Huy Ng. M <minhhuy150894@users.noreply.github.com>
2015-12-13 13:06:37 +0700

    Create download-bitmap-from-url.java

---
+++ download-bitmap-from-url.java
@@ -0,0 +1,15 @@
+public Bitmap getBitmapFromURL(String src) {
+        try {
+            java.net.URL url = new java.net.URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
